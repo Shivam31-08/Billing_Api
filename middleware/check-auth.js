7b5ea6ed78315bcca79a1f93c69aec5b30c9d68e
Shivam31-08 <87666095+Shivam31-08@users.noreply.github.com>
2022-07-04 16:50:22 +0530

    Added authentication and orders

---
+++ middleware/check-auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports=(req,res,next)=>{
+    try{
+        // const token = req.body.token;
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token,process.env.SECRET_KEY)
+        req.userData = decoded;
+        next();
+    }
+    catch (error) {
+        res.json("Auth Failed")
+        next(error)
+      }
+}
--- routes/api.route.js
+++ routes/api.route.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const {PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient()
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const checkAuth = require("../middleware/check-auth");
+
 // get Products 
-router.get("/products",async(req,res,next)=>{
+router.get("/products",checkAuth,async(req,res,next)=>{
   try {
     const products = await prisma.product.findMany({})
     res.json(products)
@@ -62,38 +67,129 @@ router.delete('/products/:id', async (req, res, next) => {
         id:Number(id)
       }
     })
-    res.json("Delted the product")
+    res.json("Deleted the product")
   } catch (error) {
     next(error);
   }
 });
 
 // Orders
+// get Products 
+router.get("/orders",async(req,res,next)=>{
+  try {
+    const orders = await prisma.order.findMany({})
+    res.json(orders)
+  } catch (error) {
+    next(error)
+  }
+})
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
-});  
+// create a order
+router.post('/orders', async (req, res, next) => {
+  try {
+    const order = await prisma.order.create({
+      data: req.body
+    })
+    res.json(order)
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get a specific order
+router.get('/order/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const order = await prisma.product.findUnique({
+     where : {id:Number(id)}
+    })
+    res.json(order)
+  } catch (error) {
+    next(error);
+  }
+});
 
 
-router.get('/user/signup',async(req,res,next)=>{
+// Delete the order
+router.delete('/orders/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const order = await prisma.order.delete({
+      where: {
+        id:Number(id)
+      }
+    })
+    res.json("Deleted the order")
+  } catch (error) {
+    next(error);
+  }
+});
+
+// User
+router.post('/user/signup',async(req,res,next)=>{
   try{
     const email = req.body.email;
     const response = await prisma.user.findUnique({
       where:{email:email}
     });
+
     if(response)
     {
       return res.json("User already exists")
     }
-    const createUser = await prisma.user.create({
-      data : req.body
-    })
-    res.json(createUser)
+    else{
+      const Hash = await bcrypt.hash(req.body.password,10);
+      const createUser = await prisma.user.create({
+          data : {
+            email : req.body.email,
+            password : Hash
+          }
+        })
+      res.json(createUser)
+    }
   }
   catch(error)
   {
     next(error)
   }
 })
+
+router.post("/user/login", async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const response = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    })
+    if (!response) {
+      return res.json("User does not exist")
+    }
+    else {
+      bcrypt.compare(req.body.password, response.password, (err, result) => {
+        if(result)
+        {
+          const token = jwt.sign({
+            id : response.id,
+            email : res.email
+          },
+          // priv key
+          process.env.SECRET_KEY,
+          {
+            expiresIn : "1h"
+          })
+          res.json("Token : "+ token)
+        }
+        else{
+          return res.json("Authentication failed")
+        }
+      })
+    }
+  } catch (error) {
+    res.json("Auth Failed")
+    next(error)
+  }
+})
+
 
 module.exports = router;
